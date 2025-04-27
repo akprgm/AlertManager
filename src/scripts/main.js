286b0bf17cd6cb0ef9217f5bd29e870bdb9440fd
Anil Kumar <akprgm@gmail.com>
2025-04-27 15:56:57 +0530

    update-app-name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,6 +14,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        title: 'IOCL: Alert Manager',
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
